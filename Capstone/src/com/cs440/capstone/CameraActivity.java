7cc05bbf34aeb63aecac8622dde9d2e65f40ce20
Patrick Green <patrickg2014@gmail.com>
2014-01-31 15:39:10 -0800

    changes
    ??>?

--- Capstone/src/com/cs440/capstone/CameraActivity.java
+++ Capstone/src/com/cs440/capstone/CameraActivity.java
@@ -1,15 +1,25 @@
 package com.cs440.capstone;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.hardware.Sensor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
 import android.widget.FrameLayout;
 
 public class CameraActivity extends Activity{
 	
 	private static Camera mainCam;
 	private CameraPreview camView;
+	@SuppressLint("NewApi")
+	android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -19,16 +29,61 @@ public class CameraActivity extends Activity{
 		camView = new CameraPreview(this, mainCam);
 		FrameLayout preview = (FrameLayout)findViewById(R.id.camera_layout);
 		preview.addView(camView);
+		Log.d("Changing", "LETS GO!");
 	}
 
 	public static Camera getCameraInstance(){
 		mainCam = null;
 		try{
+			
+			
 			mainCam = Camera.open();
+	
+			
 		}catch(Exception e){
 			//Camera is not available
 		}
 		return mainCam;
 		
-	} 
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onConfigurationChanged(android.content.res.Configuration)
+	 */
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// TODO Auto-generated method stub
+		super.onConfigurationChanged(newConfig);
+		Log.d("Changing", "is this it" + this.getResources().getConfiguration().orientation);
+		if(this.getResources().getConfiguration().orientation == 2){
+			mainCam.setDisplayOrientation(0);
+		}else{
+			mainCam.setDisplayOrientation(90);
+		}
+	}
+	
+    public static int getScreenOrientation(Activity activity) {
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int orientation = activity.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+          if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_270) {
+            return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+          } else {
+            return ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+          }
+        }
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+          if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_90) {
+            return ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+          } else {
+            return ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+          }
+        }
+        return ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
+      }
+	
+	
+	
+	
+	
 }
--- Capstone/src/com/cs440/capstone/CameraPreview.java
+++ Capstone/src/com/cs440/capstone/CameraPreview.java
@@ -2,14 +2,19 @@ package com.cs440.capstone;
 
 import java.io.IOException;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /** A basic Camera preview class */
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
     private SurfaceHolder mHolder;
     private Camera mCamera;
 
@@ -21,15 +26,21 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
+        
         // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
+    
+    
 
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
         try {
+        	Log.d("Changing", "surface created method");
             mCamera.setPreviewDisplay(holder);
+            mCamera.setDisplayOrientation(90);
             mCamera.startPreview();
+            
         } catch (IOException e) {
             Log.d("TAG", "Error setting camera preview: " + e.getMessage());
         }
@@ -37,29 +48,40 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     public void surfaceDestroyed(SurfaceHolder holder) {
         // empty. Take care of releasing the Camera preview in your activity.
+    	
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
-
+    	Log.d("Changing", "yppppppppppppksedfpdomszdfmds");
         if (mHolder.getSurface() == null){
           // preview surface does not exist
           return;
         }
 
+        if (!mHolder.getSurface().equals(android.hardware.Sensor.TYPE_ORIENTATION)){
+        	Log.d("Changing", "???");
+        }
         // stop preview before making changes
         try {
             mCamera.stopPreview();
+            Log.d("Changing", "just stopped");
         } catch (Exception e){
           // ignore: tried to stop a non-existent preview
+        	Log.d("Changing", "just stopped BADDDDDDDDDDDDDDDDDDDDDD");
         }
 
         // set preview size and make any resize, rotate or
         // reformatting changes here
-
+        // Checks the orientation of the screen for landscape and portrait and set portrait mode always
+       //if portrait.. be set at 0
+       // if landscape set at 90
+       // mCamera.setDisplayOrientation(90);
+        
         // start preview with new settings
         try {
+        	Log.d("Changing", "starting again");
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
 
@@ -67,4 +89,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             Log.d("TAG", "Error starting camera preview: " + e.getMessage());
         }
     }
+    
+
 }
