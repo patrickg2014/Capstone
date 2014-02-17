a1fc7c69db6b07d24086df9e6563f372fa41e0aa
Patrick Green <patrickg2014@gmail.com>
2014-02-16 17:48:22 -0800

    New CampusInfo class / cleanup

--- Eli's code/src/com/cs440/capstone/CameraActivity.java
+++ Eli's code/src/com/cs440/capstone/CameraActivity.java
@@ -7,21 +7,27 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Surface;
 import android.widget.FrameLayout;
 
-public class CameraActivity extends Activity{
+public class CameraActivity extends Activity implements SensorEventListener{
 	
 	private static Camera mainCam;
 	private CameraPreview camView;
+	private SensorManager mSensorManager;
+
 	@SuppressLint("NewApi")
 	android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
 
 	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.camera_layout);
@@ -29,6 +35,9 @@ public class CameraActivity extends Activity{
 		camView = new CameraPreview(this, mainCam);
 		FrameLayout preview = (FrameLayout)findViewById(R.id.camera_layout);
 		preview.addView(camView);
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+		
 		Log.d("Changing", "LETS GO!");
 	}
 
@@ -59,11 +68,23 @@ public class CameraActivity extends Activity{
 		return 0;
         
       }
+    
+    
 	
     @Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	@Override
     protected void onPause() {
         super.onPause();      // if you are using MediaRecorder, release it first
         releaseCamera();              // release the camera immediately on pause event
+        mSensorManager.unregisterListener(this);
+
     }
 
 
@@ -77,5 +98,20 @@ public class CameraActivity extends Activity{
 	
 	
     }
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		
+		float degree = Math.round(event.values[0]);
+		Log.d("Heading","Heading: " + Float.toString(degree) + " degrees");
+		
+	}
 	
 }
