111db1673ceaac818b39873b2cbab7994c701564
ChristopherLivingston92 <ChristopherLivingston92@gmail.com>
2014-03-25 19:03:08 -0700

    Added new main activity menu

--- Eli's code/src/com/cs440/capstone/MainActivity.java
+++ Eli's code/src/com/cs440/capstone/MainActivity.java
@@ -1,268 +1,39 @@
 package com.cs440.capstone;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.hardware.Camera;
-import android.location.Location;
-import android.os.Bundle;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-import android.view.Display;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-import android.app.ActionBar;
 
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
-
-@SuppressLint("NewApi")
 public class MainActivity extends Activity {
 
-	public ArrayList<ArrayList<Marker>> keepers = new ArrayList();
-	public ArrayList<ArrayList> listoflists = new ArrayList();
-	ArrayList<Marker> allMarkers = new ArrayList();
-	ArrayList<Marker> currentlyvisable = new ArrayList();
-	GoogleMap map = null;
-	
-	 private String[] mOptionTitles;
-	private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
-
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    CustomDrawerAdapter adapter;
-
-    List<DrawerItem> dataList;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) //where our app sets up
-		{
-		super.onCreate(savedInstanceState);
-		
-		Display display = getWindowManager().getDefaultDisplay();
-		int orientation = display.getRotation();
-		if(orientation==3||orientation==1)
-		{
-			finish();
-			cameraActivity();
-		}
-		setContentView(R.layout.activity_main);
-		// Map
-
-		// Get a handle to the Map Fragment
-
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))	//sets up the map view we have
-				.getMap();
-
-		CampusInfo campusInfo = new CampusInfo(map);
-		CampusInfo.createMarkers();
-		
-		 initDrawer(savedInstanceState);
-	}
-	
-	public void initDrawer(Bundle savedInstanceState){
-		mTitle = "test";
-		 
-	 	mOptionTitles = getResources().getStringArray(R.array.Menu);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mOptionTitles));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description */
-                R.string.drawer_close  /* "close drawer" description */
-        ) {
- 
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-            }
- 
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mTitle);
-            }
-        };
- 
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
- 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-
-	
-	//initialize drawer
-	dataList = new ArrayList<DrawerItem>();
-    mTitle = mDrawerTitle = getTitle();
-    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-    mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
-                GravityCompat.START);
- // Add Drawer Item to dataList
-    dataList.add(new DrawerItem("Map", R.drawable.ic_action_email));
-    dataList.add(new DrawerItem("Camera", R.drawable.ic_action_camera));
-    dataList.add(new DrawerItem("Tour", R.drawable.ic_action_gamepad));
-    dataList.add(new DrawerItem("Navigate", R.drawable.ic_action_labels));
-    dataList.add(new DrawerItem("Search", R.drawable.ic_action_search));
-    dataList.add(new DrawerItem("Facebook", R.drawable.ic_action_cloud));
-    dataList.add(new DrawerItem("About", R.drawable.ic_action_about));
-    dataList.add(new DrawerItem("Settings", R.drawable.ic_action_settings));
-    dataList.add(new DrawerItem("Help", R.drawable.ic_action_help));
-    adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item,
-            dataList);
-
-    mDrawerList.setAdapter(adapter);
-    mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-    
-    getActionBar().setDisplayHomeAsUpEnabled(true);
-    getActionBar().setHomeButtonEnabled(true);
-     
-    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.drawable.ic_drawer, R.string.drawer_open,
-                R.string.drawer_close) {
-          public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to
-                                                          // onPrepareOptionsMenu()
-          }
-     
-          public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to
-                                                          // onPrepareOptionsMenu()
-          }
-    };
-     
-    mDrawerLayout.setDrawerListener(mDrawerToggle);
-     
-    if (savedInstanceState == null) {
-          selectItem(0);
-    }
-    
-    mDrawerLayout.openDrawer(mDrawerList);
-	
-	}
-	
-	
-	public void cameraActivity() //what allows us to switch to the camera activity on button click
-	{
-		Intent intent = new Intent(this, CameraActivity.class);
-		startActivity(intent);
-	}
-
-	public boolean checkCameraHardware(Context context) {
-		if (context.getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_CAMERA)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/**
-	 * This method changes to the CameraActivity when the phone is rotated
-	 */
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-	  super.onConfigurationChanged(newConfig);
-	// Pass any configuration change to the drawer toggles
-      mDrawerToggle.onConfigurationChanged(newConfig);
-	  if(newConfig.orientation == newConfig.ORIENTATION_LANDSCAPE){
-		  cameraActivity();
-	  }
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        
-     // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-              return true;
-        }
-        return false;
-    }
-	
-	public void selectItem(int possition) {
-		 
-        mDrawerList.setItemChecked(possition, true);
-        if(dataList.get(possition).getItemName().contentEquals("Camera")){
-        	Log.d("Test", "CameraTiime");
-        	cameraActivity();
-        }
-        if(dataList.get(possition).getItemName().contentEquals("Facebook")){
-        	Log.d("Test", "CameraTiime");
-        	 Intent intent = new Intent(MainActivity.this, LoginUsingLoginFragmentActivity.class);
-             startActivity(intent);
-        	
-        }
-        if(dataList.get(possition).getItemName().contentEquals("About")){
-        	Log.d("Test", "OPENGLLLLLLLLLLL");
-        	 Intent intent = new Intent(MainActivity.this, OpenGlActivity.class);
-             startActivity(intent);
-        	
-        }
-        mDrawerLayout.closeDrawer(mDrawerList);
-
-  }
-	
-	@Override
-	public void setTitle(CharSequence title) {
-	      mTitle = title;
-	      getActionBar().setTitle(mTitle);
-	}
-	
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-	      super.onPostCreate(savedInstanceState);
-	      // Sync the toggle state after onRestoreInstanceState has occurred.
-	      mDrawerToggle.syncState();
-	}
- 
-	private class DrawerItemClickListener implements
-    ListView.OnItemClickListener {
 @Override
-public void onItemClick(AdapterView<?> parent, View view, int position,
-          long id) {
-    selectItem(position);
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+}
 
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+return true;
 }
+
+public void doSearch(View view){
+//do something when the search button is pressed
+Intent intent = new Intent(this, DoSearch.class);
+startActivity(intent);
 }
- 
+
+public void viewCampus(View view){
+Intent intent = new Intent(this,MapActivity.class);
+startActivity(intent);
+}
+
+public void takeTour(View view){
+Intent intent = new Intent(this,CameraActivity.class);
+startActivity(intent);
+}
 
 }
