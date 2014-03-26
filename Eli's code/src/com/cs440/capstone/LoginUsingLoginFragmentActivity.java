1e3b82f55b996f72eed94fe8319c2a89906239fa
espiegelups <eli@elispiegel.com>
2014-03-25 19:30:01 -0700

    FQL query update
    the query now works just need to find a good way to wait for it to
    return and parse through it

--- Eli's code/src/com/cs440/capstone/LoginUsingLoginFragmentActivity.java
+++ Eli's code/src/com/cs440/capstone/LoginUsingLoginFragmentActivity.java
@@ -16,18 +16,32 @@
 
 package com.cs440.capstone;
 
+
+
+import java.util.Arrays;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
+
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
 import com.facebook.Session;
+import com.facebook.Session.NewPermissionsRequest;
 import com.facebook.SessionState;
+import com.facebook.model.GraphObject;
 import com.facebook.widget.UserSettingsFragment;
 
 public class LoginUsingLoginFragmentActivity extends FragmentActivity {
     private UserSettingsFragment userSettingsFragment;
-
+    public static Request request;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -35,18 +49,54 @@ public class LoginUsingLoginFragmentActivity extends FragmentActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         userSettingsFragment = (UserSettingsFragment) fragmentManager.findFragmentById(R.id.login_fragment);
+        
         userSettingsFragment.setSessionStatusCallback(new Session.StatusCallback() {
             @Override
             public void call(Session session, SessionState state, Exception exception) {
-                Log.d("LoginUsingLoginFragmentActivity", String.format("New session state: %s", state.toString()));
+            	String fqlQuery = "SELECT name, venue, start_time,end_time, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE (uid IN (SELECT uid2 FROM friend WHERE uid1 = me())  OR uid = me())limit 10000) AND  end_time>now() AND venue.latitude > \"47.257379\" AND venue.latitude < \"47.265393\" AND venue.longitude < \"-122.477989\" AND venue.longitude  >\"-122.485628\"";
+            	Bundle params = new Bundle();
+            	params.putString("q", fqlQuery);
+
+            	Session session1 = Session.getActiveSession();
+           
+            	Request request = new Request(session1, 
+            	    "/fql", 
+            	    params, 
+            	    HttpMethod.GET, 
+            	    new Request.Callback(){ 
+            	        public void onCompleted(Response response) {
+            	        Log.i("fql", "Got results: " + response.toString());
+            	    }
+            	});
+            	Request.executeBatchAsync(request);
+            	
+
             }
+            
         });
+       this.request=request;
+                       
     }
+
+    
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         userSettingsFragment.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+    }
+    private Session createSession() {
+        Session activeSession = Session.getActiveSession();
+        if (activeSession == null || activeSession.getState().isClosed()) {
+            activeSession = new Session.Builder(this).setApplicationId("431689033600302").build();
+            Session.setActiveSession(activeSession);
+            activeSession.requestNewReadPermissions(new NewPermissionsRequest( userSettingsFragment, Arrays.asList("user_photos, user_events, user_friends, user_location, user_activities, friends_events")));
+        }
+        if(activeSession.isOpened())
+        {
+        	Log.i("fql", "should be open");
+        }
+        return activeSession;
     }
 
 }
